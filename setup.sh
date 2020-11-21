4b2359fb1f8e7777918ec98397102c60f71a37af
Kunal Vaidya <69197443+KunalVaidya99@users.noreply.github.com>
2020-11-21 14:28:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"170020003@iitdh.ac.in\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+mkdir -p ~/.streamlit/                                               
+echo "\                       
+[server]\n\                       
+port = $PORT\n\                       
+enableCORS = false\n\                       
+headless = true\n\                       
+\n\                       
 " > ~/.streamlit/config.toml
