49fdfb5a19575794a2443db7b5c37b69875eaabb
Kunal Vaidya <69197443+KunalVaidya99@users.noreply.github.com>
2020-11-21 12:40:39 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
-mkdir -p ~/.streamlit/                                               
-echo "\                       
-[server]\n\                       
-port = $PORT\n\                       
-enableCORS = false\n\                       
-headless = true\n\                       
-\n\                       
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"170020003@iitdh.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
